97ebd30ad2888c6e9b4698f6e027687375c5a08c
Julien Maulny <j.maulny.altimate@neopost.com>
2014-10-02 18:20:31 +0200

    fix comment controller

--- src/Brioche/CoreBundle/Controller/CommentController.php
+++ src/Brioche/CoreBundle/Controller/CommentController.php
@@ -22,53 +22,47 @@ class CommentController extends Controller
         $em = $this->getDoctrine()->getManager();
         $comments = $em->getRepository('BriocheCoreBundle:Comment')->findAllEnabledComments();
         
-        // Cache validation
-        $lastModified = (count($comments) > 0) ? $comments[0]->getDateCreate() : new \DateTime('1991-11-09');
-        $response = new Response();
-        $response
-            ->setLastModified($lastModified)
-            ->setPublic()
-        ;
-        
-        if ($response->isNotModified($request)) {
-            return $response;
+        if (!$request->isMethod('post')) {
+            // Cache validation
+            $lastModified = (count($comments) > 0) ? $comments[0]->getDateCreate() : new \DateTime('1991-11-09');
+            $response = new Response();
+            $response
+                ->setLastModified($lastModified)
+                ->setPublic()
+            ;
+
+            if ($response->isNotModified($request)) {
+                return $response;
+            }
         }
         
-        $commentForm = $this->createForm(new CommentType(), null, array(
-            'action' => $this->generateUrl('comment_post'),
-        ));
+        $commentForm = $this->createForm(new CommentType());
+        
+        if ($request->isMethod('post')) {
+            $commentForm->handleRequest($request);
+            
+            if ($commentForm->isValid()) {
+                $comment = $commentForm->getData();
+
+                if (0 === strlen(trim($comment->getAuthor()))) {
+                    $comment->setAuthor('Quelqu\'un');
+                }
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($comment);
+                $em->flush();
+
+                $this->get('brioche_core.flash_messages')->addSuccess('Commentaire ajouté !');
+
+                return $this->redirect($this->generateUrl('comment_index'));
+            } else {
+                $this->get('brioche_core.flash_messages')->addDanger('Le formulaire n\'est pas valide.');
+            }
+        }
         
         return $this->render('BriocheCoreBundle:Comment:index.html.twig', array(
             'comments' => $comments,
             'commentForm' => $commentForm->createView(),
         ), $response);
-    }
-    
-    /**
-     * @Route(
-     *      "/livre-d-or/publier-un-message",
-     *      name = "comment_post"
-     * )
-     * @Method({"POST"})
-     */
-    public function postAction(Request $request)
-    {
-        $commentForm = $this->createForm(new CommentType());
-        
-        $commentForm->handleRequest($request);
-        
-        if ($commentForm->isValid()) {
-            $comment = $commentForm->getData();
-            
-            if (0 === strlen(trim($comment->getAuthor()))) {
-                $comment->setAuthor('Quelqu\'un');
-            }
-            
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($comment);
-            $em->flush();
-            
-            return $this->redirect($this->generateUrl('comment_index'));
-        }
     }
 }
